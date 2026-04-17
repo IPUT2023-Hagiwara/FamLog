24991c9080d09334fd76845597dc7a986589f87e
iput2025-Shiga <pepio9999@gmail.com>
2026-04-17 17:28:01 +0900

    画面遷移　ホームからプロフィール5個追加、バージョン変更など行った0417

--- app/src/main/java/com/example/poilog/SleepActivity.kt
+++ app/src/main/java/com/example/poilog/SleepActivity.kt
@@ -1,54 +1,37 @@
 package com.example.poilog
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+class SleepActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val buttonNext = findViewById<Button>(R.id.button2)
-
-        buttonNext.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_sleep)
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
-
-        bottomNavigation.selectedItemId = R.id.nav_home
+        bottomNavigation.selectedItemId = R.id.nav_sleep
 
         bottomNavigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
-
                 R.id.nav_home -> {
+                    startActivity(Intent(this, MainActivity::class.java))
                     true
                 }
-
                 R.id.nav_graph -> {
                     startActivity(Intent(this, GraphActivity::class.java))
                     true
                 }
-
                 R.id.nav_calendar -> {
                     startActivity(Intent(this, CalendarActivity::class.java))
                     true
                 }
-
-                R.id.nav_sleep -> {
-                    startActivity(Intent(this, SleepActivity::class.java))
-                    true
-                }
-
+                R.id.nav_sleep -> true
                 R.id.nav_profile -> {
                     startActivity(Intent(this, ProfileActivity::class.java))
                     true
                 }
-
                 else -> false
             }
         }
