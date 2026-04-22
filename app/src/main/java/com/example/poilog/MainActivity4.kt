e13419747964617888dd42c712cceb50b335008d
iput2023-hagiwara <tk230129@t.iput.ac.jp>
2026-04-22 17:52:23 +0900

    画面推移：記録画面完了後にホームに戻る機能を追加

--- app/src/main/java/com/example/poilog/MainActivity4.kt
+++ app/src/main/java/com/example/poilog/MainActivity4.kt
@@ -1,11 +1,22 @@
 package com.example.poilog
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
+
+        val rootLayout = findViewById<View>(R.id.rootLayout)
+
+        rootLayout.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+
+            startActivity(intent)
+        }
     }
 }
